c4a0aae8b5dd0d3644f37b07737bcf724e0ce621
Sergio <Sergeagui@gmail.com>
2018-08-14 22:05:34 -0500

    updating after class

--- Lesson04/BaseProject/Startup.cs
+++ Lesson04/BaseProject/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BaseProject.Data;
 using BaseProject.Intrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -27,6 +28,7 @@ namespace BaseProject
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationContext>(opts => opts.UseSqlServer(Configuration.GetConnectionString("BaseProject")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.AddSingleton<IDateTimeProvider>(new DateTimeProvider());
         }
