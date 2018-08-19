2e9ebbce8dd6dc8ed117a24cd1706f7675e25ab4
Sergio <Sergeagui@gmail.com>
2018-08-19 18:52:08 -0500

    adding checkpoint 1 base file

---
+++ CP1/CP1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CP1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
