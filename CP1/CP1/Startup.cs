0bcdd1a8a6e2c500da8785f99cfcc05d62f40fb0
Sergio <Sergeagui@gmail.com>
2019-09-27 09:30:09 -0500

    Reverting back to development mode

--- CP1/CP1/Startup.cs
+++ CP1/CP1/Startup.cs
@@ -37,7 +37,7 @@ namespace CP1
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsProduction())
+            if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
